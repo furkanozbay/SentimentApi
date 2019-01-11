224011a70bf223edb4052a91c85bd872e7d57655
Furkan Özbay <ozbayfurkan@outlook.com>
2019-01-11 09:57:20 +0300

    Delete test app

--- sentiment-analyzer.js
+++
@@ -1,8 +0,0 @@
-
-var sentiment = require('sentiment-turkish');
- 
-var r1 = sentiment('beyler bence güzel telefon alınabilir');
-console.dir(r1);
- 
-var r2 = sentiment('arkadaşlar iyi ki almışım diyorum');
-console.dir(r2); 
